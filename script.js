a1bb803a7aaaf9c41ed9422fb2bd2b5549adefc4
jpvlif <137355102+jpvlif@users.noreply.github.com>
2023-10-05 20:14:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
-const listaConvidados = [];
+// Seleção de elementos do DOM
+const inputText = document.querySelector('#input');
+const btnAdd = document.querySelector('#add');
+const ulConvidados = document.querySelector('.convidados');
+const limpar = document.querySelector('#limpar');
 
-function validarNome(nome) {
-  return nome.match(/^[a-zA-Z]+$/);
+btnAdd.style.backgroundColor = 'green';
+limpar.style.backgroundColor = 'red';
+
+// Função para adicionar convidados à lista
+function addConvidados() {
+    const text = inputText.value.trim();
+    const regex = /^[A-Za-z\s]+$/;
+    if (text !== '' && text.match(regex)) {
+        btnAdd.textContent = 'Adicionado!';
+
+        // Criando elementos do DOM
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        const bt = document.createElement('button');
+        bt.textContent = 'Remover';
+
+        // Adicionando elementos ao DOM
+        div.classList.add('flex');
+        div.appendChild(checkbox);
+        div.appendChild(bt);
+        li.textContent = text;
+        li.appendChild(div);
+        ulConvidados.appendChild(li);
+
+        // Adicionando eventos aos botões
+        bt.addEventListener('click', () => {
+            ulConvidados.removeChild(li);
+        });
+
+        checkbox.addEventListener('change', () => {
+            li.classList.toggle('risk');
+        });
+
+        // Limpar o input e focar nele para a próxima entrada
+        inputText.value = '';
+        inputText.focus();
+
+    } else {
+        document.querySelector('h1').innerHTML = 'Inválido';
+    }
 }
 
-function atualizarLista() {
-    const ul = document.querySelector(".lista-convidados");
-    ul.innerHTML = "";
-  
-    for (const convidado of listaConvidados) {
-      const li = document.createElement("li");
-  
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.checked = false;
-      checkbox.style.float = "left";
-  
-      const button = document.createElement("button");
-      button.textContent = "Deletar";
-      button.style.float = "right";
-  
-      li.appendChild(checkbox);
-      li.appendChild(document.createTextNode(convidado));
-      li.appendChild(button);
-  
-      ul.appendChild(li);
+// Função para limpar toda a lista de convidados
+function limparAll() {
+    while (ulConvidados.firstChild) {
+        ulConvidados.removeChild(ulConvidados.firstChild);
     }
-  }
-  
+    btnAdd.textContent = 'Adicionar';
+}
 
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const nome = document.querySelector("input[name='nome']").value;
-
-  if (!validarNome(nome)) {
-    alert("Verifique as informações do convidado, o valor inserido não é válido.");
-    return;
-  }
-
-  listaConvidados.push(nome);
-  atualizarLista();
+// Evento de tecla Enter no input
+inputText.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        addConvidados();
+    }
 });
 
-document.querySelector(".lista-convidados").addEventListener("click", (event) => {
-    const target = event.target;
-  
-    if (target.tagName === "INPUT" && target.type === "checkbox") {
-      const li = target.closest("li");
-  
-      li.classList.toggle("riscado");
-    } else if (target.tagName === "BUTTON" && target.textContent === "Deletar") {
-      const li = target.closest("li");
-      const index = listaConvidados.indexOf(li.textContent);
-      listaConvidados.splice(index, 1);
-  
-
-      atualizarLista();
-    }
-  });
+// Adicionando eventos aos botões principais
+btnAdd.addEventListener('click', addConvidados);
+limpar.addEventListener('click', limparAll);
